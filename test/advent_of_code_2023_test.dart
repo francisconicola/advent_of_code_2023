3e370d8b024e86471e0a44dd3106aee32c8ece0e
francisconicola <49844500+francisconicola@users.noreply.github.com>
2023-12-06 00:42:29 -0300

    Fix input file name

--- test/advent_of_code_2023_test.dart
+++ test/advent_of_code_2023_test.dart
@@ -7,12 +7,12 @@ import 'package:test/test.dart';
 void main() {
   group('day 01', () {
     test('part 1 example', () async {
-      final file = File('input/01/day_01_part_1_example.txt');
+      final file = File('input/01/part_1.txt');
       final solution = await day_01.solveFirstPart(file.toLineStream());
       expect(solution, 142);
     });
     test('part 2 example', () async {
-      final file = File('input/01/day_01_part_2_example.txt');
+      final file = File('input/01/part_2.txt');
       final solution = await day_01.solveSecondPart(file.toLineStream());
       expect(solution, 281);
     });
