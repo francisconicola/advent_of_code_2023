d00836855c80d9d39a489f988cf103da5a7cb5ac
francisconicola <49844500+francisconicola@users.noreply.github.com>
2023-12-08 02:50:00 -0300

    Refactor tests into test builder

--- test/advent_of_code_2023_test.dart
+++ test/advent_of_code_2023_test.dart
@@ -6,43 +6,29 @@ import 'package:advent_of_code_2023/src/day_02.dart' as day_02;
 import 'package:advent_of_code_2023/src/day_03.dart' as day_03;
 import 'package:test/test.dart';
 
+typedef Solver = Future<int> Function(Stream<String>);
+
+Future<void> Function() buildTest(String inputFilepath, Solver solver, Object? expectedSolution) {
+  return () async {
+    final file = File(inputFilepath);
+    final solution = await solver(file.toLineStream());
+    expect(solution, expectedSolution);
+  };
+}
+
 void main() {
   group('day 01', () {
-    test('part 1 example', () async {
-      final file = File('input/01/part_1.txt');
-      final solution = await day_01.solveFirstPart(file.toLineStream());
-      expect(solution, 142);
-    });
-    test('part 2 example', () async {
-      final file = File('input/01/part_2.txt');
-      final solution = await day_01.solveSecondPart(file.toLineStream());
-      expect(solution, 281);
-    });
+    test('part 1 example', buildTest('input/01/part_1.txt', day_01.solveFirstPart, 142));
+    test('part 2 example', buildTest('input/01/part_2.txt', day_01.solveSecondPart, 281));
   });
 
   group('day 02', () {
-    test('part 1 example', () async {
-      final file = File('input/02/part_1.txt');
-      final solution = await day_02.solveFirstPart(file.toLineStream());
-      expect(solution, 8);
-    });
-    test('part 2 example', () async {
-      final file = File('input/02/part_2.txt');
-      final solution = await day_02.solveSecondPart(file.toLineStream());
-      expect(solution, 2286);
-    });
+    test('part 1 example', buildTest('input/02/part_1.txt', day_02.solveFirstPart, 8));
+    test('part 2 example', buildTest('input/02/part_2.txt', day_02.solveSecondPart, 2286));
   });
 
   group('day 03', () {
-    test('part 1 example', () async {
-      final file = File('input/03/part_1.txt');
-      final solution = await day_03.solveFirstPart(file.toLineStream());
-      expect(solution, 4361);
-    });
-    test('part 2 example', () async {
-      final file = File('input/03/part_2.txt');
-      final solution = await day_03.solveSecondPart(file.toLineStream());
-      expect(solution, 467835);
-    });
+    test('part 1 example', buildTest('input/03/part_1.txt', day_03.solveFirstPart, 4361));
+    test('part 2 example', buildTest('input/03/part_2.txt', day_03.solveSecondPart, 467835));
   });
 }
